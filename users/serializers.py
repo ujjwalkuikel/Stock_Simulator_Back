ae8a5f8b9a10ee434407526ea1519671ba7c53e6
Joshua D. Nodler <nodlerjo@msu.edu>
2024-11-19 18:04:58 -0500

    Fixed the implementation of Django password strength validation.

--- users/serializers.py
+++ users/serializers.py
@@ -2,6 +2,8 @@
 from rest_framework import serializers
 from .models import CustomUser
 from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 
 User = get_user_model()
 class CustomUserSerializer(serializers.ModelSerializer):
@@ -9,6 +11,14 @@ class CustomUserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['email', 'password', 'id', 'first_name', 'last_name']
         extra_kwargs = {'password': {'write_only': True}}
+    
+    def validate(self, data):
+        password = data.get('password')
+        try:
+            validate_password(password)
+        except ValidationError as e:
+            raise serializers.ValidationError(e.messages)
+        return data
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
